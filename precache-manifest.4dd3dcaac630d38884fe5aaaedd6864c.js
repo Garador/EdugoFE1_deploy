ec7f135e1ceb25f7e2203b5280e239ec6a188753
azolot <azolotdev@gmail.com>
2021-06-07 22:05:16 -0400

    Updates

---
+++ precache-manifest.4dd3dcaac630d38884fe5aaaedd6864c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47979ddcf3a8d0a5dcc8f76d35899ab8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e67cdcd7fe2c2efe5fe",
+    "url": "./static/css/main.54beb3a5.chunk.css"
+  },
+  {
+    "revision": "053ff638a18e6bac0b19",
+    "url": "./static/js/2.c5c42987.chunk.js"
+  },
+  {
+    "revision": "71a47aec01097e1e4a161ab65b112a32",
+    "url": "./static/js/2.c5c42987.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e67cdcd7fe2c2efe5fe",
+    "url": "./static/js/main.af044673.chunk.js"
+  },
+  {
+    "revision": "8866326dcd7c00cd7d8e",
+    "url": "./static/js/runtime-main.2633ec5b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/EdugoFE1_deploy/precache-manifest.57250ebc9803847dcfb1f5ee95a6e76a.js"
+  "./precache-manifest.4dd3dcaac630d38884fe5aaaedd6864c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EdugoFE1_deploy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
